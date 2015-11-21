53bc4d34b1a10607ded46c5c07638eb31a4e2c5a
Michael Castanieto <mcastanieto@gmail.com>
2015-11-21 12:42:08 -0800

    cleaned up and fixed getMaxError and interpolate functions

--- getMaxError.c
+++ getMaxError.c
@@ -10,22 +10,16 @@ struct breakPoint getMaxError(float data[], float env[], int length, int nhar1) 
 
   struct breakPoint current;
 
-  printf("\nInside getMaxError()\n");
   for (i = 0; i < length; i++) {
     temp = abs(env[i] - data[i*nhar1]);
     if (max < temp) {
       max = temp;
       index = i;
-      //printf(" Current max: %8.2f \n", max);
-    } else {
-      //printf("\n NO NEW MAX: %d", i);
     }
   }
 
   current.amplitude = max;
-  current.index = temp;
-
-  printf("Max amp error: %8.2f, Index: %d \n ", max, temp);
+  current.index = index;
 
   return current;
 }
